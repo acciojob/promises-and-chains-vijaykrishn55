56d0a23159065d2720c7a5b2672360a5aefd1a59
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-18 18:43:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+      const userForm = document.getElementById('userForm');
+      const ageInput = document.getElementById('age');
+      const nameInput = document.getElementById('name');
+      const submitButton = document.getElementById('btn');
+
+      userForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const age = parseInt(ageInput.value);
+        const name = nameInput.value;
+
+        if (!age || !name) {
+          alert('Please fill in all fields.');
+          return;
+        }
+
+        const promise = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (age >= 18) {
+              resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+              reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+          }, 4000);
+        });
+
+        promise
+          .then((message) => {
+            alert(message);
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      });
+    });
